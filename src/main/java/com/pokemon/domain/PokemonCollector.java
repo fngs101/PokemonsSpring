d9f3ad681a15b846102d4fdd5d3201d554fae908
jszymczak <jszymczak@xtm-intl.com>
2022-09-17 21:12:49 +0200

    Use html template for all of the htmls

--- src/main/java/com/pokemon/domain/PokemonCollector.java
+++ src/main/java/com/pokemon/domain/PokemonCollector.java
@@ -1,7 +1,10 @@
 package com.pokemon.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import java.util.List;
 
 @Entity
@@ -10,6 +13,12 @@ public class PokemonCollector
     @Id
     private int id;
     private String userName;
+    @OneToOne(targetEntity=Card.class)
     private List<Card> cardList;
     private int pokemonCoin;
+
+    private PokemonCollector()
+    {
+
+    }
 }
